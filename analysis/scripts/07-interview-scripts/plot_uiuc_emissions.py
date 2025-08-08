43d9c1d5cbe3c7a758c772b8824c0723a5d33dd8
Sam Dotson <samgdotson@gmail.com>
2025-08-07 22:09:20 -0500

    updates emissions plot to include IL, adds PCAP, and starts planning processes section

--- analysis/scripts/07-interview-scripts/plot_uiuc_emissions.py
+++ analysis/scripts/07-interview-scripts/plot_uiuc_emissions.py
@@ -26,19 +26,31 @@ if __name__ == "__main__":
     df = tables[0].iloc[:,:2]
     df = df[::-1].set_index(pd.to_datetime(df['Applicable Date'][::-1]).dt.year)\
                  .drop(columns=['Applicable Date'])
-    df.columns = ['Actual Emissions']
+    df.columns = ['UIUC Actual']
 
     future = tables[-1]
     future = future.set_index(pd.to_datetime(future['Year'], format="%Y"))\
                     .drop(columns=['Year'])\
                     .resample('YE').mean().interpolate(method='linear')
     future.index = future.index.year
+    future.columns = ['UIUC Target']
     
+    # ==== Illinois emissions ====
+    il_df = pd.DataFrame({'year':[2005,2021,2030,2050],
+                      'emissions':[283.63e6, 228.01e6,135e6,0]}
+                     )
+    il_df['year'] = pd.to_datetime(il_df['year'], format="%Y")
+    il_df.set_index('year',inplace=True,drop=True)
+    il_df = il_df.resample('YE').mean().interpolate('linear')
+    il_df.index = il_df.index.year
+    il_df.columns = ['IL State Target']
 
 
     fig, ax = plt.subplots(figsize=(8,6))
     df.plot(ax=ax, marker='o')
     future.plot(ax=ax, linestyle='--', color='tab:green')
+    # plot illinois emissions
+    il_df.plot(ax=ax, linestyle='-.', color='tab:purple')
     ax.set_xlim(2008,2050)
     # ax.set_ylim(0,600e3)
     ax.set_yscale('log')
